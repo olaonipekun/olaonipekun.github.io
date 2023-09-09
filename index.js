4269addee12d674734b87bc2e42b286b6c8d52c0
Ololade <olaonipekunololade94@gmail.com>
2023-09-09 07:33:15 +0100

    now trying with chrome

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ heading.onmouseenter = (ev) => {
     heading.textContent = "please leave me alone for now!!";
     heading.style.color = "red";
 }
+
+heading.onmouseleave = () => {
+    heading.textContent = "Hello Everyone";
+    heading.style.color = 'coral';
+}
