027ae6b5fbf5aafe876c4f6b9272c6fb1e2bae34
Ololade <olaonipekunololade94@gmail.com>
2023-09-09 19:32:59 +0100

    adding json and making changes

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const heading = document.getElementById('heading');
-const imageDiv = document.getElementById("profileImage");
 
-console.log(heading);
-console.log(imageDiv);
 
 heading.onmouseenter = (ev) => {
     heading.textContent = "please leave me alone for now!!";
@@ -13,16 +10,4 @@ heading.onmouseleave = (ev) => {
     heading.textContent = "Hello Everyone";
     heading.style.color = "coral";
 }
-
-const imageLoaded = () => {
-    fetch("https://slack.com/api/users.profile.get")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(err => console.log(err))
-}
-
-document.addEventListener(onload, imageLoaded)
-
 
