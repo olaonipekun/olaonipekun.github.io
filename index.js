7c2e4e6f6edd38ccc67f99c27e82a1ffb96678de
Ololade <olaonipekunololade94@gmail.com>
2023-09-10 08:24:52 +0100

    good changes

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ heading.onmouseleave = (ev) => {
     heading.style.color = "coral";
 }
 
+fetch("http://127.0.0.1:5500/user.json")
+.then((res) => res.json())
+.then((data) => {
+    console.log(data)
+    let profileImage = document.getElementById("profileImage")
+    console.log(profileImage)
+    let datas = data
+    console.log(datas.Profile[0].image)
+    profileImage.setAttribute('src', `${data.Profile[0].image}`)
+})
+.catch(err => console.log(err))
