1a19ccdc90915ead07e9022dc60b3d0959b3a91f
Ololade <37458073+olaonipekun@users.noreply.github.com>
2023-09-11 16:48:08 +0100

    changes

--- index.js
+++ index.js
@@ -1,70 +1,33 @@
 let dateText = document.getElementById("dateText")
-dateText.classList.add("text_edit")
-
 let utcTimeText = document.getElementById("utcTimeText")
+let heading = document.getElementById('heading');
+let slackName = document.getElementById("slackName")
+let profileImage = document.getElementById("profileImage")
+let gitHubUrl = document.getElementById("gitHubUrl")
+let currentDate = document.getElementById("currentDate")
+let utcTime = document.getElementById("utcTime")
+
+
+utcTime.classList.add("data_edit")
+slackName.classList.add("text_edit")
+gitHubUrl.classList.add("data_edit")
+dateText.classList.add("text_edit")
 utcTimeText.classList.add("text_edit")
+heading.classList.add("text_edit");
 
-// Fetching Data from user.json file
-fetch("./user.json")
-  .then((res) => res.json())
-  .then((data) => {
+// UTC DATE AND TIME
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let d = new Date()
+let day = weekday[d.getDay()]
+currentDate.textContent = day;
+currentDate.classList.add("data_edit")
 
-    // putting the fetched data in it's own variable
-    let datas = data.Profile[0]
+// Getting UTC TIME IN MILLISECONDS    
+function updateUTC() {
+  let getTime = new Date().getTime();
+  utcTime.textContent = getTime
+}
+updateUTC();
+setInterval(updateUTC, 1000);
 
-    // getting the DOM elements
-    let heading = document.getElementById('heading');
-    let slackName = document.getElementById("slackName")
-    let profileImage = document.getElementById("profileImage")
-    let gitHubUrl = document.getElementById("gitHubUrl")
-    gitHubUrl.classList.add("data_edit")
-    let currentDate = document.getElementById("currentDate")
-    let utcTime = document.getElementById("utcTime")
-    utcTime.classList.add("data_edit")
 
-    // setting slack name and attribute
-    slackName.textContent = datas.slack_name
-    slackName.style.fontSize = "25px";
-    slackName.setAttribute('data-testid', `${datas.slack_name_attribute}`)
-    slackName.style.color = "coral"
-    slackName.classList.add("text_edit");
-
-    // setting heading as track and fixing track attribute
-    heading.style.color = "coral";
-    const track = datas.track
-    const trackText = document.createTextNode(track)
-    heading.appendChild(trackText)
-    heading.setAttribute('data-testid', `${datas.track_attribute}`)
-    heading.classList.add("text_edit");
-
-    // getting profile image and setting attribute
-    profileImage.setAttribute('src', `${datas.image}`)
-    profileImage.setAttribute('data-testid', `${datas.image_attribute}`)
-    profileImage.setAttribute('alt', `${datas.alt}`)
-
-    // UTC DATE AND TIME
-    const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let d = new Date()
-    let day = weekday[d.getDay()]
-    currentDate.textContent = day;
-    currentDate.setAttribute('data-testid', `${datas.currentDayOfTheWeek}`)
-    currentDate.classList.add("data_edit")
-
-    // Getting UTC TIME IN MILLISECONDS    
-    function updateUTC() {
-      let getTime = new Date().getTime();
-      utcTime.textContent = getTime
-    }
-    updateUTC();
-    setInterval(updateUTC, 1000);
-
-   // setting github url data and attribute
-   let gitHubA = document.createElement("a")
-   let gitHubText = document.createTextNode("Check out the github repo")
-   gitHubA.appendChild(gitHubText)
-   gitHubA.title = "github repo"
-   gitHubA.href = `${datas.github_url}`
-   gitHubUrl.appendChild(gitHubA)
-   gitHubA.setAttribute('data-testid', `${datas.github_url_attribute}`)
-  })
-  .catch(err => console.log(err))
